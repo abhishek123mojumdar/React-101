a8604f8cd785a4b4fd8389693f71248e5ac9848d
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-10-26 11:35:10 -0700

    1 file modified

--- src/FEclassComponent/FEclassroom.js
+++ src/FEclassComponent/FEclassroom.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FEstyle.css';
 
 export default function FEclassroom(props) {
+  let [val, setVal] = useState(10);
+
+  function setValue(e) {
+    setVal(e.target.value); // this is how we implement two way binding
+  }
+
   return (
     <React.Fragment>
       <p>
         The name is {props.name} whose divison is {props.div}
         {props.children}
       </p>
+      <input type="text" value={val} onChange={setValue} />
+      <p>{val}</p>
     </React.Fragment>
   );
 }
+
+// React fragments are used to remove the unnecessary extra node like a DIV <> </>
+
+// What is two way binding
