bd7029aeba6ee752eac25931821ced0edb5165ae
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-10-30 10:26:22 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 
 const App = () => {
   return (
-    <div>
+    <div> 
       <Greet />
       <FEclassroom name="Andrew" div={3} animals={['Dog', 'Cat']} />
       <UseEffComp />
