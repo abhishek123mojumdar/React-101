c99cfd126269c9065ee6f3b9f24f07fbdd06a3eb
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-10-30 10:38:09 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,14 +4,39 @@ import FEclassroom from './FEclassComponent/FEclassroom';
 import Greet from './IntroClassComponent/GreetClassComponent';
 import UseEffComp from './FEclassComponent/useEffectHook';
 import PropTypes from 'prop-types';
+import { Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div> 
-      <Greet />
-      <FEclassroom name="Andrew" div={3} animals={['Dog', 'Cat']} />
-      <UseEffComp />
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Greet (Lifecycle hooks)</Link>
+        </li>
+        <li>
+          <Link to="/classRoom">Class (Fragments and porpTypes)</Link>
+        </li>
+        <li>
+          <Link to="/hook">Hooks (Fragments and porpTypes)</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Greet />}></Route>
+        <Route
+          path="/classRoom"
+          element={
+            <FEclassroom name="Andrew" div={3} animals={['Dog', 'Cat']} />
+          }
+        ></Route>
+        <Route path="/hook" element={<UseEffComp />}></Route>
+      </Routes>
+    </>
+    // <div>
+    //   <Greet />
+    //   <FEclassroom name="Andrew" div={3} animals={['Dog', 'Cat']} />
+    //   <UseEffComp />
+    // </div>
   );
 };
 
