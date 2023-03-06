8175ec68715e6a6a6185d3cf1975a98fd2150368
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2023-03-06 10:57:13 +0530

    2 files modified

--- src/FEclassComponent/FormsComponent.js
+++ src/FEclassComponent/FormsComponent.js
@@ -13,9 +13,14 @@ const FormsComp = (props) => {
   const [userDetailArr, setUserDetailArr] = useState([]);
   const [error, setError] = useState({ message: '' });
   const [errorHasOccured, setErrorHasOccured] = useState(false);
+  const [myInputData, setMyInputData] = useState('');
+  const [showData, setShowData] = useState('');
 
   function handleInputs(e) {
-    console.log(e.target.name + ' --- ' + e.target.value);
+    console.log(e.target.name);
+    console.log(e.target.value);
+    console.log(e.target.placeholder);
+    //The name that is provided in the input boxes is same as the attributes of the object {formData} which manages the form; as we get the input `name` we can use that as a key and set/update the key value of the formObject
     //console.log(e.target.value);
     const name = e.target.name;
     setFormData({ ...formData, [name]: e.target.value });
@@ -66,9 +71,33 @@ const FormsComp = (props) => {
     setError({ message: messageVal });
   }
 
+  const manageMyInput = (event) => {
+    //console.log(event.target.value);
+    //myInputData
+    setMyInputData(event.target.value);
+  };
+
+  const showMyInputData = () => {
+    setShowData(myInputData);
+  };
+
   return (
     <React.Fragment>
       <p>Welcome to forms component</p>
+      <hr />
+
+      <input
+        type="text"
+        name="myInput"
+        value={myInputData}
+        onChange={manageMyInput}
+      />
+      <span> Hey! wazzup. {showData}</span>
+      {/* to handle form values we need to use onChange event handler. If we do not use that we will not be able to get the values from 
+      form inputs. If just use the value prop without the onChange event handler then the input field becomes readonly. */}
+
+      <button onClick={showMyInputData}> show data </button>
+      <hr />
       <form action="" onSubmit={handleSubmit}>
         <div>
           <label>
@@ -76,6 +105,7 @@ const FormsComp = (props) => {
             <input
               type="text"
               name="name"
+              placeholder="Enter Name"
               value={formData.name}
               onChange={handleInputs}
             />
@@ -85,6 +115,7 @@ const FormsComp = (props) => {
             <input
               type="text"
               name="age"
+              placeholder="Enter age"
               value={formData.age}
               onChange={handleInputs}
             />
@@ -94,6 +125,7 @@ const FormsComp = (props) => {
             <input
               type="text"
               name="phNumber"
+              placeholder="enter phone number"
               value={formData.phNumber}
               onChange={handleInputs}
             />
@@ -103,6 +135,7 @@ const FormsComp = (props) => {
             <input
               type="text"
               name="email"
+              placeholder="enter email"
               value={formData.email}
               onChange={handleInputs}
             />
